ab24c690d12a5302debe0039363cc1d9ca9dd9f0
Denis Kochetkov <denis_ka_2307@mail.ru>
2026-09-18 14:44:46 +0300

    add base for tests

---
+++ src/tests.py
@@ -0,0 +1,19 @@
+import unittest
+import app
+
+class AppTests(unittest.TestCase):
+	def test_create(self):
+		pass
+	def test_get(self):
+		pass
+	def test_get_by_id(self):
+		pass
+	def test_post(self):
+		pass
+	def test_detach(self):
+		pass
+	def test_delete(self):
+		pass
+
+if __name__ == "__main__":
+	unittest.main()
